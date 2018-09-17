f25c2966a74e3ac2130b96997a23219bb8348867
the-c0d3r <z3r0.mhu@gmail.com>
2018-09-17 21:27:31 +0800

    ascii.c: learn that char is in fact int

---
+++ ascii.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * @brief this is to test out the character's value that is
+ * convertable from int to str
+ */
+int main(){
+
+  int i;
+
+    for(i=0;i<=255;i++)
+         printf("%c: %d\n",i,i);
+
+    return 0;
+}
